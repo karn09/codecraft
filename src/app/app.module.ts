a78db81a635af20260d1d0b55ca61beef2ad14d6
John Nieves <karn09@gmail.com>
2017-05-26 09:50:15 -0400

    initial codecraft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginButtonComponent } from './header/login-button/login-button.component';
+import { BarFooComponent } from './header/login-button/bar-foo/bar-foo.component';
+import { MyDirective } from './header/my.directive';
+import { MyPipe } from './header/my.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginButtonComponent,
+    BarFooComponent,
+    MyDirective,
+    MyPipe
   ],
   imports: [
     BrowserModule,
